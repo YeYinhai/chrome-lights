1140bc9b0a8da3bb46fdf267ebe5c71a2ad91093
yyh <362127358@qq.com>
2018-09-05 10:28:03 +0800

    初始化。版本2.5

---
+++ js/content.js
@@ -0,0 +1,70 @@
+(function () {
+    // 首次
+    var first = true;
+    var firstLength = contentLength();
+
+    var config = {
+        isContent: false,
+        isFoot: true
+    }
+
+    chrome.storage.sync.get(["isContent","isFoot"], function (data) {
+        // alert(JSON.stringify(data));
+        config = data;
+    })
+
+
+    chrome.extension.onMessage.addListener(
+        // 直接接受popup发来的消息
+        function (request, sender, sendResponse) {
+            config = request;
+            main();
+            sendResponse({ result: "ok" })
+        }
+    );
+
+    setInterval(() => {
+        if (first) {
+            main();
+            first = false;
+        } else if (firstLength != contentLength()) {
+            main();
+            firstLength = contentLength();
+        }
+    }, 200)
+
+    function main() {
+        // 去掉底部公司信息。
+        config.isFoot ? $('#foot').hide() : $('#foot').show();
+
+        if (config.isContent) {
+            // 去掉普通内容
+            $('.c-abstract').hide();
+            // 去掉带有图片的内容
+            $('.c-span-last > p > p ').hide();
+        } else {
+            $('.c-abstract').show();
+            $('.c-span-last > p > p ').show();
+        }
+
+        // 去掉头部广告
+        $('.m').each((index, element) => {
+            if (element.innerText == '广告') {
+                $(element).parent().parent().hide();
+            }
+        });
+
+        // 设置容器高度
+        $('#content_left').css({ "overflow": "auto", "padding-top": "20px", "width": "640px" });
+        $('#content_right').css({ "overflow": "auto", "max-height": "850px", "margin-top": "150px" });
+        $('#wrapper_wrapper').css({ "padding-bottom": "100px" });
+        // 分页悬浮
+        $('#page').css({ "position": "fixed", "bottom": "0" });
+        $('#page>a').css({ "height": "36px" });
+    }
+
+    function contentLength() {
+        return $('#content_left')[0].innerText.length;
+    }
+
+})();
---
+++ js/popup.js
@@ -0,0 +1,70 @@
+
+var config = {}
+
+window.onload = function () {
+    chrome.storage.sync.get(["isContent", "isFoot"], function (data) {
+        // alert(JSON.stringify(data));
+        config = data;
+        document.getElementById('isContent').checked = data.isContent;
+        document.getElementById('isFoot').checked = data.isFoot;
+    })
+
+}
+
+document.querySelectorAll('input').forEach((element) => {
+    element.onclick = function () {
+        config[this.id] = this.checked;
+        sendMessageToContentScript(config, (response) => {
+            if (response.result == "ok") {
+                // alert(JSON.stringify(config));
+                chrome.storage.sync.set(config, function (data) { })
+            }
+        });
+    }
+});
+
+
+// document.getElementById('isContent').onclick = function () {
+//     config.isContent = this.checked;
+//     sendMessageToContentScript(config, (response) => {
+//         if (response.result == "ok") {
+//             // alert(1);
+//         }
+//     });
+// }
+
+// 获取当前选项卡ID
+function getCurrentTabId(callback) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (callback) {
+            // alert(tabs.length, tabs[0].id)
+            callback(tabs.length ? tabs[0].id : null);
+        }
+    });
+}
+
+// 这2个获取当前选项卡id的方法大部分时候效果都一致，只有少部分时候会不一样
+function getCurrentTabId2() {
+    chrome.windows.getCurrent(function (currentWindow) {
+        chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (tabs) {
+            if (callback) callback(tabs.length ? tabs[0].id : null);
+        });
+    });
+}
+
+
+// 向content-script主动发送消息
+function sendMessageToContentScript(message, callback) {
+    getCurrentTabId((tabId) => {
+        chrome.tabs.sendMessage(tabId, message, function (response) {
+            if (callback) callback(response);
+        });
+    });
+}
+
+// 向content-script注入JS片段
+function executeScriptToCurrentTab(code) {
+    getCurrentTabId((tabId) => {
+        chrome.tabs.executeScript(tabId, { code: code });
+    });
+}
